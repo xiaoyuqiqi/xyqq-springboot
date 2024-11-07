39e3a4dad44a2d263092ff266121183128a6f4cd
lizhiqi <Qi199346@>
2024-11-07 15:13:05 +0800

    feat: 新增security-oauth2资源服务器集成方式

---
+++ xyqq-security-oauth2-resource/src/main/java/com/xyqq/XyqqSecurityOauth2ResourceApplication.java
@@ -0,0 +1,13 @@
+package com.xyqq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyqqSecurityOauth2ResourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XyqqSecurityOauth2ResourceApplication.class, args);
+    }
+
+}
