3ce68da9a922b05b93122e74d4eae87e4685337c
lizhiqi <Qi199346@>
2024-11-08 17:57:42 +0800

    feat: 新增redis模块

---
+++ xyqq-springboot-redis/src/main/java/com/xyqq/XyqqSpringbootRedisApplication.java
@@ -0,0 +1,14 @@
+package com.xyqq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class XyqqSpringbootRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XyqqSpringbootRedisApplication.class, args);
+    }
+
+}
