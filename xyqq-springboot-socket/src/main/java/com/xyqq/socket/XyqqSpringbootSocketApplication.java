25270ebd1fe6be31e2b86667ed310ccc9ac24373
lizhiqi <Qi199346@>
2025-02-13 09:31:25 +0800

    feat: 新增socket模块

---
+++ xyqq-springboot-socket/src/main/java/com/xyqq/socket/XyqqSpringbootSocketApplication.java
@@ -0,0 +1,13 @@
+package com.xyqq.socket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyqqSpringbootSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyqqSpringbootSocketApplication.class, args);
+	}
+
+}
