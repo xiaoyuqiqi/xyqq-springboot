696f3ae50711f76dd4b781c081a950e40569b944
lizhiqi <Qi199346@>
2025-02-13 09:31:31 +0800

    feat: 新增socket模块

---
+++ xyqq-springboot-socket/src/main/java/com/xyqq/socket/controller/WebSocketController.java
@@ -0,0 +1,18 @@
+package com.xyqq.socket.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WebSocketController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public String greeting(String message) throws Exception {
+        // 模拟一个处理过程
+        Thread.sleep(1000);
+        return "Hello, " + message + "!";
+    }
+}
+
